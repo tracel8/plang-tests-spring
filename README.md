00795709bfc167ccabf7519978058c272e2bc5b5
Evan Derby <somekidwithhtml@gmail.com>
2019-04-09 12:37:21 -0600

    Added instructions, code from Jeff

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # plang-tests-spring
 Put Tests for Programming Languages here
+
+## Installing HUnit
+
+When you downloaded Haskell, if you chose the all-in-one installer ("Haskell Platform") you should already have the `cabal` tool, which you can use from the command line. Ensure Cabal is up to date with `cabal --version` and then run `cabal install HUnit`.
+
+## Importing Tests
+
+In order to run the tests, first clone this repository onto your local machine. Then, from the GHCi, load your assignment file first, then the respective testing file:
+
+```
+Prelude> :l Assignment1/a1c.hs
+[1 of 1] Compiling A1c
+...
+*A1c> :add plang-tests-spring/a1_tests.hs
+```
+
+Make sure you use the `:add` command and not the `:l` command twice! If you use the `:l` command twice, the tests will not be able to import your code. 
+
+## Running Tests
+
+```
+*Main> runTestTT tests
+-> results
+```
--- a1_tests.hs
+++ a1_tests.hs
@@ -1,8 +1,28 @@
-sDotProduct 5 (3,4) (4,5)
---- = 160
+import Test.HUnit
+-- notice that we specify the file here as a module to import its bindings, thus
+-- Haskell looks for a file called a1c.hs in its current working folder
+import A1c
+          
+-- this defines the set of tests, notice it's a big list!     
+tests = test [
+  -- 1. sDotProduct
+  "sDotProduct 5 (0, 0) (0, 0)" ~: 0 ~=? (sDotProduct 5 (0, 0) (0, 0)),
 
-distance (0,0) (3,4)
---- = 5
+  -- 2. distance
+  "distance (0, 1) (0, 0)" ~: 1.0 ~=? (distance (0, 1) (0, 0)),
 
-tripleDistance (0,0,0) (1,2,2)
---- = 3
+  -- 3. tripleDistance
+  "tripleDistance (0, 0, 1) (0, 0, 0)" ~: 1.0 ~=? (tripleDistance (0, 0, 1) (0, 0, 0)),
+
+  -- 4. findMin
+  "findMin [2, 3, 1]" ~: 1 ~=? (findMin [2, 3, 1]),
+
+  -- 5. tupleDotProduct
+  "tupleDotProduct [1, 1, 1] [1, 2, 3]" ~: 6 ~=? (tupleDotProduct [1, 1, 1] [1, 2, 3]),
+
+  -- 6. revZip2Lists
+  "revZip2Lists [1, 2, 3] ['a', 'b', 'c']" ~: [('c', 3), ('b', 2), ('a', 1)] ~=? (revZip2Lists [1, 2, 3] ['a', 'b', 'c']),
+
+  -- 7. everyThird
+  "everyThird [1..30]" ~: [3,6,9,12,15,18,21,24,27,30] ~=? (everyThird [1..30])
+  ]
