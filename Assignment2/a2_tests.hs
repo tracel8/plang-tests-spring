07f786a78e98e6d2d585a6db13426d8342aafb44
Evan Derby <somekidwithhtml@gmail.com>
2019-04-11 18:09:08 -0600

    Add assignment 2 tests partial

---
+++ Assignment2/a2_tests.hs
@@ -0,0 +1,24 @@
+import Test.HUnit
+
+import A2c
+
+tests = test [
+  -- 1. removeAllExcept
+  "removeAllExcept 'a' ['a', 'b', 'c', 'a']" ~: ['a', 'a'] ~=? (removeAllExcept 'a' ['a', 'b', 'c', 'a']),
+  "removeAllExcept 1 [1, 2, 3, 4, 5]" ~: [1] ~=? (removeAllExcept 1 [1, 2, 3, 4, 5]),
+  "removeAllExcept 1.5 [1, 1.5, 2, 1.5, 1]" ~: [1.5, 1.5] ~=? (removeAllExcept 1.5 [1, 1.5, 2, 1.5, 1]),
+
+  -- 2. removeAll
+  "removeAll 'a' ['a', 'b', 'c', 'a']" ~: ['b', 'c'] ~=? (removeAll 'a' ['a', 'b', 'c', 'a']),
+  "removeAll 1 [1, 2, 3, 4, 5]" ~: [2, 3, 4, 5] ~=? (removeAll 1 [1, 2, 3, 4, 5]),
+  "removeAll 1.5 [1, 1.5, 2, 1.5, 1]" ~: [1, 2, 1] ~=? (removeAll 1.5 [1, 1.5, 2, 1.5, 1]),
+
+  -- 3. substitute
+  "substitute 'a' 'b' ['a', 'b', 'c', 'a']" ~: ['b', 'b', 'c', 'b'] ~=? (substitute 'a' 'b' ['a', 'b', 'c', 'a']),
+  "substitute 1 2 [1, 2, 3, 4, 5]" ~: [2, 2, 3, 4, 5] ~=? (substitute 1 2 [1, 2, 3, 4, 5]),
+  "substitute 1.5 1 [1, 1.5, 2, 1.5, 1]" ~: [1, 1, 2, 1, 1] ~=? (substitute 1.5 1 [1, 1.5, 2, 1.5, 1]),
+
+  -- 4. mergeSorted3
+  "mergeSorted3 [2, 4, 6] [3, 7, 9] [1, 8, 5]" ~: [1..9] ~=? (mergeSorted3 [2, 4, 6] [3, 7, 9] [1, 8, 5]),
+  "mergeSorted3 ['a', 'c', 'e'] ['k', 'i', 'd'] ['b', 'r', 'o']" ~: "abcdeijor" ~=? (mergeSorted3 ['a', 'c', 'e'] ['k', 'i', 'd'] ['b', 'r', 'o'])
+]
