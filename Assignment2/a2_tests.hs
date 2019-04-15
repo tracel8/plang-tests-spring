f4b421836a6e53e268b160897e77ac30045eea3e
Trace Levinson <trace.levinson@du.edu>
2019-04-15 15:29:48 -0600

    Fix extra comma in merge, add tree for testing

--- Assignment2/a2_tests.hs
+++ Assignment2/a2_tests.hs
@@ -1,6 +1,30 @@
 import Test.HUnit
-
 import A2b
+
+alpha:: TriTree Char
+alpha =
+    TriNode 'a'
+        (TriNode 'b'
+            (TriNode 'l'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 'u'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 'x'
+                EmptyNode EmptyNode EmptyNode))
+        (TriNode 'c'
+            (TriNode 'r'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 'q'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 'p'
+                EmptyNode EmptyNode EmptyNode))
+        (TriNode 'k'
+            (TriNode 'v'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 's'
+                EmptyNode EmptyNode EmptyNode) 
+            (TriNode 'y'
+                EmptyNode EmptyNode EmptyNode))
 
 tests = test [
   -- 1. removeAllExcept
@@ -20,5 +44,8 @@ tests = test [
 
   -- 4. mergeSorted3
   "mergeSorted3 [2, 4, 6] [3, 7, 9] [1, 5, 8]" ~: [1..9] ~=? (mergeSorted3 [2, 4, 6] [3, 7, 9] [1, 5, 8]),
-  "mergeSorted3 [1.5, 2.5, 3.5] [2, 7.5, 8.5], [3, 9.5, 11.5]" ~: [1.5, 2, 2.5, 3, 3.5, 7.5, 8.5, 9.5, 11.5] ~=? (mergeSorted3 [1.5, 2.5, 3.5] [2, 7.5, 8.5], [3, 9.5, 11.5]),
-  "mergeSorted3 ['a', 'c', 'e'] ['d', 'i', 'k'] ['b', 'o', 'r']" ~: "abcdeikor" ~=? (mergeSorted3 ['a', 'c', 'e'] ['d', 'i', 'k'] ['b', 'o', 'r'])]
+  "mergeSorted3 [1.5, 2.5, 3.5] [2, 7.5, 8.5], [3, 9.5, 11.5]" ~: [1.5, 2, 2.5, 3, 3.5, 7.5, 8.5, 9.5, 11.5] ~=? (mergeSorted3 [1.5, 2.5, 3.5] [2, 7.5, 8.5] [3, 9.5, 11.5]),
+  "mergeSorted3 ['a', 'c', 'e'] ['d', 'i', 'k'] ['b', 'o', 'r']" ~: "abcdeikor" ~=? (mergeSorted3 ['a', 'c', 'e'] ['d', 'i', 'k'] ['b', 'o', 'r']),
+
+  -- leaflist
+  "leafList alpha" ~: "luxrqpvsy" ~=? (leafList alpha)]
